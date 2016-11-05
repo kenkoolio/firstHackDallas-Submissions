a025f0fd5cc019588294dbd0c788a65c52ce03c9
Melissa Guerrero <melissamguerrero@hotmail.com>
2016-11-05 16:29:53 -0500

    routes

--- Coding_Mojo/Red_Ribbon/config/routes.rb
+++ Coding_Mojo/Red_Ribbon/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+=======
+  get 'pages' => 'pages#index'
+  root 'pages#index'
+>>>>>>> f6be491c7bbcdd313701c14b6e46f9c7a82e8216
 
   resources :service_to_types
   resources :service_types
@@ -8,6 +13,19 @@ Rails.application.routes.draw do
   resources :zips
   resources :addresses
   resources :service_providers
+<<<<<<< HEAD
+=======
+
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+>>>>>>> f6be491c7bbcdd313701c14b6e46f9c7a82e8216
 
   get 'pages' => 'pages#index'
 
