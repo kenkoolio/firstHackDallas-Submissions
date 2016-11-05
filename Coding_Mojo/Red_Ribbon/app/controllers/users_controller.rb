abadd618b67fc6108b138accfb6bbcd3f9bf5738
Melissa Guerrero <melissamguerrero@hotmail.com>
2016-11-05 15:54:52 -0500

    users new

--- Coding_Mojo/Red_Ribbon/app/controllers/users_controller.rb
+++ Coding_Mojo/Red_Ribbon/app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+	def new
+	end
+
+	def index
+   		@users = User.all
+  	end
+
+  	def show
+    	@user = User.find(params[:id])
+  	end
+  
+  	def new
+    	@user = User.new
+  	end
+
+  	def create
+    	@user = User.new(user_params)
+    	if @user.save
+    		session[:user_id] = @user.id
+      		redirect_to "/users"
+    	else
+      		flash[:errors] = @user.errors.full_messages
+      		redirect_to "/users/new"
+    	end
+  	end
+
+  	def edit
+    	@user = User.find(params[:id])
+ 	end
+
+  	def update
+    	@user = User.find(params[:id])
+    	if @user.update_attributes(user_params)
+      		redirect_to "/users"
+    	else 
+      		flash[:errors] = @user.errors.full_messages
+      		redirect_to "/users/#{@user.id}/edit"
+    	end
+  	end
+
+  	private
+
+  	def user_params
+    	params.require(:user).permit(:first_name, :last_name, :email, :password)
+  	end
 end
--- Coding_Mojo/Red_Ribbon/config/routes.rb
+++ Coding_Mojo/Red_Ribbon/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :zips
   resources :addresses
   resources :service_providers
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
